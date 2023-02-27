909df558c82ec64afe704d8888a4cc514d8f1f9e
Tushar Dhiman <tushar.dhiman@knoldus.com>
2023-02-27 17:46:50 +0530

    Added files for solid principles

---
+++ SOLID PRINCIPLES Assignment/MainArea.java
@@ -0,0 +1,80 @@
+import java.util.Scanner;
+
+class Rectangle{
+    int m_width;
+    int m_height;
+   
+    public void setWidth(int width){
+      m_width = width;
+    }
+   
+    public void setHeight(int h) {
+      m_height = h;
+    }
+   
+    public int getWidth() {
+      return m_width;
+    }
+   
+    public int getHeight() {
+      return m_height;
+    }
+   
+    public int getArea() {
+      return m_width * m_height;
+    }
+   
+  }
+  
+  class Square extends Rectangle {
+
+    public void setWidth (int width) {
+      setSide(width);
+    }
+   
+    public void setHeight (int height) {
+      setSide(height);
+    }
+
+
+    public void setSide(int side){
+
+        m_width = side;
+        m_height = side;
+    }
+  }
+
+  class MainArea{
+
+    public static void main(String...s){
+
+        
+        Rectangle rec_obj = new Rectangle();
+        Square square_obj = new Square();
+        Scanner input_object = new Scanner(System.in);
+
+        System.out.println("Enter the height and width of the rectangle to find it's area");
+
+        int height = input_object.nextInt();
+
+        int width = input_object.nextInt();        
+
+        rec_obj.setHeight(height);
+        rec_obj.setWidth(width);    
+        
+        System.out.println("Enter the side of the square");
+
+        int side = input_object.nextInt();
+
+        square_obj.setWidth(side);
+        square_obj.setHeight(side);
+
+        System.out.println("Area of the sqaure is = " +square_obj.getArea());
+        System.out.println("Area of rectangle is = " +rec_obj.getArea());
+
+        input_object.close();
+
+
+
+    }
+  }
